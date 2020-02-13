e1f365af184e953bd97636574f9a1a1db8447350
AIR625988492 <625988492@qq.com>
2020-02-13 15:53:10 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// app.use(cors({
+//     origin: "http://localhost:8080"
+//     exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+//     maxAge: 5,
+//     credentials: true,
+//     allowMethods: ['GET', 'POST', 'DELETE'],
+//     allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+// }
+// ));
